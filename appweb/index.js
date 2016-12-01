0e470f1b50937cee960141277260ec5e70e3ec1d
Ytallo de Lima Gadelha <ytallolg@gmail.com>
2016-12-01 02:16:18 -0300

    Conhecendo jQuery e Axios

--- appweb/index.js
+++ appweb/index.js
@@ -7,6 +7,8 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use(bodyParser.json());
+
 app.get("/dosave", (req,res) => {
   console.log("os dados recebidos do formulário são:");
   console.log(req.query);
@@ -24,6 +26,11 @@ app.post("/adivinha", (req,res) => {
 	console.log("os dados recebidos do formulário são:");
 	console.log(req.body);
 	res.send("<h1>Sucesso!</h1> <a href='numero.html'>voltar</a>");
+});
+
+app.get("/convidados", (req,res) => {
+  var convs = [{nomeconvidado:"Maria"},{nomeconvidado:"João"}];
+  res.send(convs);
 });
 
 app.listen(3000);
