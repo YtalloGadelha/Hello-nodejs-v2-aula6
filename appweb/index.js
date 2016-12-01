622ae723ceba8f5d3678d04e929f7c31eb1ccbf6
Ytallo de Lima Gadelha <ytallolg@gmail.com>
2016-12-01 01:42:39 -0300

    Exercício com método POST(numero.html)

--- appweb/index.js
+++ appweb/index.js
@@ -1,17 +1,29 @@
 // index.js
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
+const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
 
-app.get("/hello", (req,res) => res.send("hello"));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/dosave", (req,res) => {
   console.log("os dados recebidos do formulário são:");
   console.log(req.query);
   res.send("<h1>Sucesso!</h1><a href='index.html'>voltar</a>");
+});
+
+app.post("/doSave", (req,res) => {
+  console.log("os dados recebidos do formulário são:");
+  console.log(req.body);
+  res.send("<h1>Sucesso!</h1><a href='index.html'>voltar</a>");
+});
+
+app.post("/adivinha", (req,res) => {
+
+	console.log("os dados recebidos do formulário são:");
+	console.log(req.body);
+	res.send("<h1>Sucesso!</h1> <a href='numero.html'>voltar</a>");
 });
 
 app.listen(3000);
